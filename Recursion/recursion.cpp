5eb7915482144aab272a1f9bc1cc8e228f64b0cd
Taeil-Nam <tnam@student.42seoul.kr>
2024-01-18 11:07:12 +0900

    Add: add first recursion

---
+++ Recursion/recursion.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+/*
+Recursion structure
+
+recursion(parameter)
+{
+	if (baseCondition)
+	{
+		~~~ // Ascending statement.
+		recursion(...) // Recursion.
+		~~~ // Descending statement.
+	}
+}
+*/
+
+void recursion(int number)
+{
+	if (number <= 0)
+	{
+		return;
+	}
+	std::cout << number << std::endl;	// Ascending statement. (재귀 함수 호출 시점에 실행됨)
+	recursion(number - 1);
+	std::cout << "recursion(" << number << ") finished." << std::endl;	// Descending statement. (재귀 함수 탈출 시점에 실행됨)
+}
+
+int main()
+{
+	int number = 5;
+
+	recursion(number);
+
+	return 0;
+}
