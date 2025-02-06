b46895f07a19c30a151a7cf0ab99304d9b118129
Taeil Nam <nam0314@gmail.com>
2025-02-06 21:20:56 +0900

    Modify: baekjoon_1253 hashMap->twoPointer

--- KD/personal/baekjoon_1253.cpp
+++ KD/personal/baekjoon_1253.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,18 +14,21 @@ int main(){
         cin >> num;
         nums.push_back(num);
     }
+    sort(nums.begin(), nums.end());
     int res = 0;
-    for (int i = 0; i < nums.size(); i++){
-        unordered_map<int, int> freq;
+    for (int i = 0; i < N; i++){
         int target = nums[i];
-        for (int j = 0; j < nums.size(); j++){
-            if (j == i)
-                continue;
-            if (freq.count(target - nums[j]) > 0){
+        int left = 0;
+        int right = N - 1;
+        while (left < right){
+            if (left == i) {left++; continue;}
+            if (right == i) {right--; continue;}
+            if (nums[left] + nums[right] == target){
                 res++;
                 break;
             }
-            freq[nums[j]]++;
+            else if (nums[left] + nums[right] < target) left++;
+            else if (nums[left] + nums[right] > target) right--;
         }
     }
     cout << res;
